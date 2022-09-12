eb6b4e24f1e829cba6e66076448e7fd69f4d66c9
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-09-12 13:35:58 -0500

    added expenses components and create props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import logo from "./logo.svg";
-import "./App.css";
 
-import ExpenseItem from "./components/ExpenseItem";
+import "./App.css";
+import Expenses from "./components/Expenses";
+
+
 
 function App() {
   const expenses = [
@@ -11,7 +12,12 @@ function App() {
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+    },
     {
       id: "e3",
       title: "Car Insurance",
@@ -25,32 +31,10 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
-
-  return (
-    <div>
-      <h2>Let's get started</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
-    </div>
-  );
+  return <div>
+    <h2> Let's get started</h2>
+    <Expenses items={expenses} />
+  </div>;
 }
 
 export default App;
