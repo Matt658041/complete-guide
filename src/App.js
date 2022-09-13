f0ea006a2187f4aa152b394746a68c74667e156a
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-09-13 10:36:26 -0500

    added new expense form and css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-
+import React from "react";
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 
 
@@ -32,7 +33,7 @@ function App() {
     },
   ];
   return <div>
-    <h2> Let's get started</h2>
+    <NewExpense/>
     <Expenses items={expenses} />
   </div>;
 }
