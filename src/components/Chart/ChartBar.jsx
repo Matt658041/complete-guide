13380334089bb20abbdf1dde0fce6cf49b15998f
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-09-14 15:14:35 -0500

    created barfillheight var

--- src/components/Chart/ChartBar.jsx
+++ src/components/Chart/ChartBar.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import './ChartBar.css';
+import React from "react";
+import "./ChartBar.css";
 
-const ChartBar = () => {
+const ChartBar = (props) => {
+    let barFillHeight = '0%'
+
   return (
-    <div>ChartBar</div>
-  )
-}
+    <div className="chart-bar">
+      <div className="chart-bar__inner">
+        <div className="chart-bar__fill"></div>
+      </div>
+          <div className="chart-bar__label">{props.label}</div>
+    </div>
+  );
+};
 
-export default ChartBar
+export default ChartBar;
