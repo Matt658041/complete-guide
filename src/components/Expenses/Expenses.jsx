abf4e0d141d297aaebfa654072d1d6f7538e94fb
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-09-14 12:03:08 -0500

    created expenses list jsx and css component

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -8,11 +8,24 @@ const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
 
   const filterChangeHandler = (selectedYear) => {
-        setFilteredYear(selectedYear);
+    setFilteredYear(selectedYear);
   };
-  const filteredExpenses = props.items.filter(expense => {
+  const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
-  })
+  });
+
+  let expensesContent = <p>No Expenses found.</p>
+
+  if (filteredExpenses.length > 0 )  {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }; 
 
   return (
     <div>
@@ -21,14 +34,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+       {expensesContent}
       </Card>
     </div>
   );
---
+++ src/components/Expenses/ExpensesList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ExpenseList.css'
+
+const ExpensesList = () => {
+  return (
+    <div>ExpensesList</div>
+  )
+}
+
+export default ExpensesList
